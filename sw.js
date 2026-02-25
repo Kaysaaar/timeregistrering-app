987ae5746df097cadee5f65fa2b5c03156770fc9
Kaysaaar <michael.jensen91@hotmail.com>
2026-02-25 21:54:12 +0100

    sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,26 @@
-self.addEventListener('install', e => self.skipWaiting());
-self.addEventListener('activate', e => self.clients.claim());
+const CACHE = "timer-wrapper-v1";
+const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(fetch(event.request));
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (e) => {
+  const url = new URL(e.request.url);
+
+  // Cache wrapper-filerne (GitHub Pages)
+  if (url.origin === location.origin) {
+    e.respondWith(
+      caches.match(e.request).then((hit) => hit || fetch(e.request))
+    );
+    return;
+  }
+
+  // Alt andet (fx script.google) -> bare netværk (ikke cache)
+  e.respondWith(fetch(e.request));
 });
